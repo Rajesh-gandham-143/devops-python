30334a9de58bbb19f3b053c27875d07927fee820
Rajesh-gandham-143 <gandamrajesh143@gmail.com>
2024-10-26 15:32:16 +0530

    file-1

---
+++ python-01.py
@@ -0,0 +1,11 @@
+import docker
+
+client=docker.from_env()
+
+def count_containers():
+    containers=client.containers.list()
+    print("running containers")
+    for i,container in enumerate(containers):
+        print(f"{i}: {container.name} (ID: {container.short_id})")
+        return containers
+    
